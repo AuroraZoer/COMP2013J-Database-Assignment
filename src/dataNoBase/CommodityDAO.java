cb46c390dc6a214e072c56b95f8031974364aa61
Aurora-Zoe <1911977514@qq.com>
2023-04-25 20:32:21 +0800

    feat: add getCommoditiesByCategory method in CommodityDAO.java

--- src/dataNoBase/CommodityDAO.java
+++ src/dataNoBase/CommodityDAO.java
@@ -68,6 +68,27 @@ public class CommodityDAO {
         }
     }
 
-
+    // 这个方法根据类别筛选商品，每页显示10条商品信息，需要传入页码pageNumber
+    public static List<Commodity> getCommoditiesByCategory(String category, int pageNumber) {
+        List<Commodity> commodities = new ArrayList<>();
+        String sql = "SELECT * FROM commodity WHERE category = ? LIMIT 10 OFFSET ?";
+        try (Connection conn = JDBCTool.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, category);
+            pstmt.setInt(2, (pageNumber - 1) * 10);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    int cid = rs.getInt("cid");
+                    String itemName = rs.getString("itemName");
+                    float price = rs.getFloat("price");
+                    int stock = rs.getInt("stock");
+                    commodities.add(new Commodity(cid, itemName, category, price, stock));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return commodities;
+    }
 
 }
