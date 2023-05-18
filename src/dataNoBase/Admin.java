bc2667585949c4a973660769b5e10b3013c76b63
Aurora-Zoe <1911977514@qq.com>
2023-05-18 11:10:09 +0800

    feat: add type

--- src/dataNoBase/Admin.java
+++ src/dataNoBase/Admin.java
@@ -1,11 +1,12 @@
 package dataNoBase;
 
 public class Admin extends Person {
+
     public Admin() {
-        super();
+        setType(0); // Set the type as 0 for admin
     }
 
     public Admin(int id, String name, String password, String email, java.sql.Timestamp createTime) {
-        super(id, name, password, email, createTime);
+        super(id, name, password, email, createTime, 0); // Set the type as 0 for admin
     }
 }
--- src/dataNoBase/User.java
+++ src/dataNoBase/User.java
@@ -1,11 +1,12 @@
 package dataNoBase;
 
 public class User extends Person {
+
     public User() {
-        super();
+        setType(1); // Set the type as 1 for user
     }
 
     public User(int id, String name, String password, String email, java.sql.Timestamp createTime) {
-        super(id, name, password, email, createTime);
+        super(id, name, password, email, createTime, 1); // Set the type as 1 for user
     }
 }
