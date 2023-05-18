6aeb42600c89a178c33a19c5e53b236eff4bcde5
Aurora-Zoe <1911977514@qq.com>
2023-05-18 10:59:09 +0800

    fix: DAO

--- src/dataNoBase/AdminDAO.java
+++ src/dataNoBase/AdminDAO.java
@@ -2,16 +2,30 @@ package dataNoBase;
 
 import java.util.List;
 
-public class AdminDAO extends PersonDAO {
+public class AdminDAO {
 
-    public AdminDAO() {
-        super("admin", "aid", "admin_name");
+    public static void insertAdmin(Admin admin) {
+        PersonDAO.setTableName("admin");
+        PersonDAO.insertPerson(admin);
     }
 
-    @Override
-    public List<Person> getAllPersons() {
-        return super.getAllPersons(); // No need to override, as it's already implemented in the parent class
+    public static List<Person> getAllAdmins() {
+        PersonDAO.setTableName("admin");
+        return PersonDAO.getAllPersons();
     }
 
-    // Other specific methods for AdminDAO
+    public static Admin getAdminByUsername(String username) {
+        PersonDAO.setTableName("admin");
+        return (Admin) PersonDAO.getPersonByName(username);
+    }
+
+    public static boolean isPasswordCorrect(String inputUsername, String inputPassword) {
+        PersonDAO.setTableName("admin");
+        return PersonDAO.isPasswordCorrect(inputUsername, inputPassword);
+    }
+
+    public static void deleteAdminByUsername(String username) {
+        PersonDAO.setTableName("admin");
+        PersonDAO.deletePersonByName(username);
+    }
 }
