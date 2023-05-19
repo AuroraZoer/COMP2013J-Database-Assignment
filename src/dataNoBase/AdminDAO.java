434188a0154b3fbcfa659e278fd2c2079d901e9b
Aurora-Zoe <1911977514@qq.com>
2023-05-19 17:40:08 +0800

    fix: add getPersonByPage

--- src/dataNoBase/AdminDAO.java
+++ src/dataNoBase/AdminDAO.java
@@ -3,48 +3,50 @@ package dataNoBase;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AdminDAO extends PersonDAO{
+public class AdminDAO {
+
     public static void insertAdmin(Admin admin) {
-        setTableName("admin");
-        setNameColumnName("admin_name");
-        insertPerson(admin);
+        PersonDAO.setTableName("admin");
+        PersonDAO.setNameColumnName("admin_name");
+        PersonDAO.insertPerson(admin);
     }
 
     public static List<Person> getAllAdmins() {
-        setTableName("admin");
-        setIdColumnName("aid");
-        setNameColumnName("admin_name");
-        return getAllPersons();
+        PersonDAO.setTableName("admin");
+        PersonDAO.setIdColumnName("aid");
+        PersonDAO.setNameColumnName("admin_name");
+        return PersonDAO.getAllPersons();
     }
 
     public static Admin getAdminByUsername(String username) {
-        setTableName("admin");
-        setIdColumnName("aid");
-        setNameColumnName("admin_name");
-        return (Admin) getPersonByName(username);
+        PersonDAO.setTableName("admin");
+        PersonDAO.setIdColumnName("aid");
+        PersonDAO.setNameColumnName("admin_name");
+        return (Admin) PersonDAO.getPersonByName(username);
     }
 
     public static boolean isPasswordCorrect(String inputUsername, String inputPassword) {
-        setTableName("admin");
-        setNameColumnName("admin_name");
-        return isPasswordCorrect(inputUsername, inputPassword);
+        PersonDAO.setTableName("admin");
+        PersonDAO.setNameColumnName("admin_name");
+        return PersonDAO.isPasswordCorrect(inputUsername, inputPassword);
     }
 
     public static void deleteAdminByUsername(String username) {
-        setTableName("admin");
-        setNameColumnName("admin_name");
-        deletePersonByName(username);
+        PersonDAO.setTableName("admin");
+        PersonDAO.setNameColumnName("admin_name");
+        PersonDAO.deletePersonByName(username);
     }
 
-    public static List<Admin> getAdminsByPage(int pageNumber) {
-        setTableName("user");
-        setIdColumnName("uid");
-        setNameColumnName("username");
-        List<Person> persons = getPersonsByPage(pageNumber);
+    public static List<Admin> getaAdminsByPage(int pageNumber) {
+        PersonDAO.setTableName("admin");
+        PersonDAO.setIdColumnName("aid");
+        PersonDAO.setNameColumnName("admin_name");
+        List<Person> persons = PersonDAO.getPersonsByPage(pageNumber);
         List<Admin> admins = new ArrayList<>();
         for (Person person : persons) {
             admins.add((Admin) person);
         }
         return admins;
     }
+
 }
--- src/dataNoBase/UserDAO.java
+++ src/dataNoBase/UserDAO.java
@@ -3,44 +3,44 @@ package dataNoBase;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserDAO extends PersonDAO{
+public class UserDAO{
     public static void insertUser(User user) {
-        setTableName("user");
-        setNameColumnName("username");
-        insertPerson(user);
+        PersonDAO.setTableName("user");
+        PersonDAO.setNameColumnName("username");
+        PersonDAO.insertPerson(user);
     }
 
     public static List<Person> getAllUsers() {
-        setTableName("user");
-        setIdColumnName("uid");
-        setNameColumnName("username");
-        return getAllPersons();
+        PersonDAO.setTableName("user");
+        PersonDAO.setIdColumnName("uid");
+        PersonDAO.setNameColumnName("username");
+        return PersonDAO.getAllPersons();
     }
 
     public static User getUserByUsername(String username) {
-        setTableName("user");
-        setIdColumnName("uid");
-        setNameColumnName("username");
-        return (User) getPersonByName(username);
+        PersonDAO.setTableName("user");
+        PersonDAO.setIdColumnName("uid");
+        PersonDAO.setNameColumnName("username");
+        return (User) PersonDAO.getPersonByName(username);
     }
 
     public static boolean isPasswordCorrect(String inputUsername, String inputPassword) {
-        setTableName("user");
-        setNameColumnName("username");
-        return isPasswordCorrect(inputUsername, inputPassword);
+        PersonDAO.setTableName("user");
+        PersonDAO.setNameColumnName("username");
+        return PersonDAO.isPasswordCorrect(inputUsername, inputPassword);
     }
 
     public static void deleteUserByUsername(String username) {
-        setTableName("user");
-        setNameColumnName("username");
-        deletePersonByName(username);
+        PersonDAO.setTableName("user");
+        PersonDAO.setNameColumnName("username");
+        PersonDAO.deletePersonByName(username);
     }
 
     public static List<User> getUsersByPage(int pageNumber) {
-        setTableName("user");
-        setIdColumnName("uid");
-        setNameColumnName("username");
-        List<Person> persons = getPersonsByPage(pageNumber);
+        PersonDAO.setTableName("user");
+        PersonDAO.setIdColumnName("uid");
+        PersonDAO.setNameColumnName("username");
+        List<Person> persons = PersonDAO.getPersonsByPage(pageNumber);
         List<User> users = new ArrayList<>();
         for (Person person : persons) {
             users.add((User) person);
