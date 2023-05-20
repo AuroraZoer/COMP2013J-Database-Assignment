67c3cf3a773b937e337345214e90dde90fbaca6e
Aurora-Zoe <1911977514@qq.com>
2023-05-20 22:51:26 +0800

    feat: add TransactionDAO

--- src/dataNoBase/TransactionDAO.java
+++ src/dataNoBase/TransactionDAO.java
@@ -25,22 +25,16 @@ public class TransactionDAO {
         }
     }
 
-    // 计算交易总额
-    private static float calculateTotal(int cid, int quantity) {
-        String sql = "SELECT price FROM commodity WHERE cid = ?";
+    // 删除交易记录
+    public static void deleteTransaction(int tid) {
+        String sql = "DELETE FROM `transaction` WHERE tid = ?";
         try (Connection conn = JDBCTool.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setInt(1, cid);
-            ResultSet resultSet = pstmt.executeQuery();
-
-            if (resultSet.next()) {
-                float price = resultSet.getFloat("price");
-                return price * quantity;
-            }
+            pstmt.setInt(1, tid);
+            pstmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return 0;
     }
 
     // 获取指定用户的交易记录
@@ -69,6 +63,79 @@ public class TransactionDAO {
         return transactions;
     }
 
+    // 修改交易记录的商品数量和总价
+    public static void updateTransactionQuantity(int tid, int quantity) {
+        String sql = "UPDATE `transaction` SET quantity = ?, total = ? WHERE tid = ?";
+        try (Connection conn = JDBCTool.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            // 获取原始交易记录信息
+            Transaction transaction = getTransactionById(tid);
+            if (transaction == null) {
+                return; // 交易记录不存在，不进行更新
+            }
 
+            // 计算新的总价
+            float newTotal = calculateTotal(transaction.getCid(), quantity);
+
+            pstmt.setInt(1, quantity);
+            pstmt.setFloat(2, newTotal);
+            pstmt.setInt(3, tid);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 根据交易记录ID获取单个交易记录
+    public static Transaction getTransactionById(int tid) {
+        String sql = "SELECT * FROM `transaction` WHERE tid = ?";
+        try (Connection conn = JDBCTool.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, tid);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    int uid = rs.getInt("uid");
+                    int cid = rs.getInt("cid");
+                    int quantity = rs.getInt("quantity");
+                    float total = rs.getFloat("total");
+                    boolean isPaid = rs.getBoolean("is_paid");
+                    return new Transaction(tid, uid, cid, quantity, total, isPaid);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // 更新交易记录的支付状态
+    public static void updateTransactionPaymentStatus(int tid) {
+        String sql = "UPDATE `transaction` SET is_paid = 1 WHERE tid = ?";
+        try (Connection conn = JDBCTool.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, tid);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 计算交易总额
+    private static float calculateTotal(int cid, int quantity) {
+        String sql = "SELECT price FROM commodity WHERE cid = ?";
+        try (Connection conn = JDBCTool.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, cid);
+            ResultSet resultSet = pstmt.executeQuery();
+
+            if (resultSet.next()) {
+                float price = resultSet.getFloat("price");
+                return price * quantity;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 }
 
